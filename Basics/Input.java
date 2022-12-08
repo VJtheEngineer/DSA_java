e5e9e38bc05a65bc8af742896944136849a3de20
VJtheEngineer <vishnu.22120232@viit.ac.in>
2022-12-08 21:51:12 +0530

    Added Input.java

---
+++ Basics/Input.java
@@ -0,0 +1,27 @@
+
+ 
+import java.util.*;
+
+public class Input
+{
+	public static void main(String[] args) {
+	    
+	    Scanner scn = new Scanner(System.in);
+	    
+        String str1 = scn.next();
+ 
+        System.out.println("Entered String str1 : " + str1);
+ 
+        String str2 = scn.next();
+ 
+        System.out.println("Entered String str2 : " + str2);
+ 
+        int x = scn.nextInt();
+ 
+        System.out.println("Entered Integer : " + x);
+ 
+        float f = scn.nextFloat();
+ 
+        System.out.println("Entered FloatValue : " + f);
+	}
+}
